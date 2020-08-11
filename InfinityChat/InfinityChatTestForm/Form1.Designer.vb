89cfea3700891ed273bad9f310b62ae126235270
EpicDev <EytraRP@gmail.com>
2020-08-10 20:26:48 -0400

    Update Icon

--- InfinityChat/InfinityChatTestForm/Form1.Designer.vb
+++ InfinityChat/InfinityChatTestForm/Form1.Designer.vb
@@ -95,8 +95,8 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Left
-        Me.Button1.Location = New System.Drawing.Point(98, 13)
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Location = New System.Drawing.Point(684, 13)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(98, 28)
         Me.Button1.TabIndex = 3
